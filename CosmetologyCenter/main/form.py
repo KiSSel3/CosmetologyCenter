5f3ec809e4d2709341ca1b497bef5d85bbcb5922
KiSSel3 <andreyk042004@gmail.com>
2023-10-20 23:31:31 +0300

    First lab

--- CosmetologyCenter/main/form.py
+++ CosmetologyCenter/main/form.py
@@ -4,7 +4,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class ReviewForm(forms.ModelForm):
     author = forms.CharField(max_length=20, initial='Anonym')
-    text = forms.CharField(label='text', widget= forms.TextInput(attrs={'class':'form-input'}))
+    text = forms.CharField(label='text', widget= forms.TextInput(attrs={'class':'form-input'}), required=False)
     rating = forms.IntegerField(label='rating', widget= forms.NumberInput(attrs={'class': 'form-input', 'min': 0, 'max': 10}),  validators=[
         MinValueValidator(0, message="The number cannot be less than 0"),
         MaxValueValidator(10, message="The number cannot be more than 10")
@@ -12,4 +12,4 @@ class ReviewForm(forms.ModelForm):
 
     class Meta:
       model = Review
-      fields = ['text', 'rating', 'author'] 
+      fields = ['author', 'rating', 'rating'] 
